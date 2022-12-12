f6728bcea5527bba4f814b6e58769c7489a625aa
Siigmaf <faby.tbe@gmail.com>
2022-12-12 16:13:10 -0300

    desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -7,8 +7,8 @@ CASE
      THEN REPLACE( M.nome_musica, 'Amar','Code Review')
    WHEN M.nome_musica LIKE '%Pais%' 
      THEN REPLACE( M.nome_musica, 'Pais','Pull Requests')
-   WHEN M.nome_musica LIKE '%SOUl%' 
-     THEN REPLACE( M.nome_musica, 'SOUl','CODE')
+   WHEN M.nome_musica LIKE '%SOUL%' 
+     THEN REPLACE( M.nome_musica, 'SOUL','CODE')
    WHEN M.nome_musica LIKE '%SUPERSTAR%' 
      THEN REPLACE( M.nome_musica, 'SUPERSTAR','SUPERDEV')
 END AS 'novo_nome'
@@ -17,7 +17,7 @@ SpotifyClone.Musica AS M
 WHERE M.nome_musica LIKE '%Bard%'
 OR M.nome_musica LIKE '%Amar%'
 OR M.nome_musica LIKE '%Pais%'
-OR M.nome_musica LIKE '%SOUl%'
+OR M.nome_musica LIKE '%SOUL%'
 OR M.nome_musica LIKE '%SUPERSTAR%'
 ORDER By nome_musica DESC;
 
